fce6b3719bd1d885c78f0cf0a8e7e2f181c3d827
Divyam <divyam2003ds@gmail.com>
2025-01-15 11:07:28 +0530

    updated code

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-def connect_to_db(db_path='/home/vr-dt-100/Desktop/chinook.db'):
+def connect_to_db(db_path='/home/vr-dt-100/Desktop/database.db'):
     """
     Connect to the SQLite database at the given path. Handles errors gracefully and checks if the file exists.
 
